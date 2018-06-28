a2325759b614d8fd73802ab4b1e3288fa56b371b
Jorick <j.o.sijtsma@st.hanze.nl>
2018-06-28 16:36:36 +0200

    bug fixes

--- saltstackmasterfiles/slavevm_nagios_minion_add.sh
+++ saltstackmasterfiles/slavevm_nagios_minion_add.sh
@@ -10,13 +10,13 @@ sudo mkdir /usr/local/nagios/etc/servers/
 sudo touch /usr/local/nagios/etc/servers/"$minion_name".cfg
 echo "define host {
         use                          linux-server
-        host_name                    "$minion_name"
-        alias                        "$minion_name"
-        address                      "$minion_ip"
+        host_name                    $minion_name
+        alias                        $minion_name
+        address                      $minion_ip
         register                     1
 }
 define service {
-      host_name                       "$minion_name"
+      host_name                       $minion_name
       service_description             PING
       check_command                   check_ping!100.0,20%!500.0,60%
       max_check_attempts              2
@@ -31,9 +31,9 @@ define service {
       register                        1
 }
 define service {
-      host_name                       "$minion_name"
+      host_name                       $minion_name
       service_description             Check Users
-      check_command           check_local_users!20!50
+      check_command                   check_local_users!20!50
       max_check_attempts              2
       check_interval                  2
       retry_interval                  2
@@ -46,7 +46,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       "$minion_name"
+      host_name                       $minion_name
       service_description             Local Disk
       check_command                   check_local_disk!20%!10%!/
       max_check_attempts              2
@@ -61,7 +61,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       "$minion_name"
+      host_name                       $minion_name
       service_description             Check SSH
       check_command                   check_ssh
       max_check_attempts              2
@@ -76,7 +76,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       "$minion_name"
+      host_name                       $minion_name
       service_description             Total Process
       check_command                   check_local_procs!250!400!RSZDT
       max_check_attempts              2
