6451f4148078b0e0011da0ddfa1b99cbc347ff1d
Jorick <j.o.sijtsma@st.hanze.nl>
2018-06-28 16:44:52 +0200

    fixed some issues and reworked what is being logged

--- saltstackmasterfiles/slavevm_nagios_minion_add.sh
+++ saltstackmasterfiles/slavevm_nagios_minion_add.sh
@@ -8,87 +8,30 @@ read -p 'Nagios Minion HostName: ' minion_name
 
 sudo mkdir /usr/local/nagios/etc/servers/
 sudo touch /usr/local/nagios/etc/servers/"$minion_name".cfg
-echo "define host {
-        use                          linux-server
-        host_name                    $minion_name
-        alias                        $minion_name
-        address                      $minion_ip
-        register                     1
+echo "
+define host {
+	use			linux-server
+	host_name		HOST_NAME
+	alias			HOST_NAME
+	address			HOST_IP
+	max_check_attempts	5
+	check_period		24x7
+	notification_interval	30
+	notification_period	24x7
 }
+
 define service {
-      host_name                       $minion_name
-      service_description             PING
-      check_command                   check_ping!100.0,20%!500.0,60%
-      max_check_attempts              2
-      check_interval                  2
-      retry_interval                  2
-      check_period                    24x7
-      check_freshness                 1
-      contact_groups                  admins
-      notification_interval           2
-      notification_period             24x7
-      notifications_enabled           1
-      register                        1
+	use			generic-service
+	host_name		HOST_NAME
+	service_description	CPU load
+	check_command		check_nrpe!check_load
 }
+
 define service {
-      host_name                       $minion_name
-      service_description             Check Users
-      check_command                   check_local_users!20!50
-      max_check_attempts              2
-      check_interval                  2
-      retry_interval                  2
-      check_period                    24x7
-      check_freshness                 1
-      contact_groups                  admins
-      notification_interval           2
-      notification_period             24x7
-      notifications_enabled           1
-      register                        1
-}
-define service {
-      host_name                       $minion_name
-      service_description             Local Disk
-      check_command                   check_local_disk!20%!10%!/
-      max_check_attempts              2
-      check_interval                  2
-      retry_interval                  2
-      check_period                    24x7
-      check_freshness                 1
-      contact_groups                  admins
-      notification_interval           2
-      notification_period             24x7
-      notifications_enabled           1
-      register                        1
-}
-define service {
-      host_name                       $minion_name
-      service_description             Check SSH
-      check_command                   check_ssh
-      max_check_attempts              2
-      check_interval                  2
-      retry_interval                  2
-      check_period                    24x7
-      check_freshness                 1
-      contact_groups                  admins
-      notification_interval           2
-      notification_period             24x7
-      notifications_enabled           1
-      register                        1
-}
-define service {
-      host_name                       $minion_name
-      service_description             Total Process
-      check_command                   check_local_procs!250!400!RSZDT
-      max_check_attempts              2
-      check_interval                  2
-      retry_interval                  2
-      check_period                    24x7
-      check_freshness                 1
-      contact_groups                  admins
-      notification_interval           2
-      notification_period             24x7
-      notifications_enabled           1
-      register                        1
+	use			generic-service
+	host_name		HOST_NAME
+	service_description	/dev/hda1 free space
+	check_command		check_nrpe!check_hda1
 }
 " | sudo tee -a /usr/local/nagios/etc/servers/$minion_name.cfg
 sudo systemctl restart nagios
